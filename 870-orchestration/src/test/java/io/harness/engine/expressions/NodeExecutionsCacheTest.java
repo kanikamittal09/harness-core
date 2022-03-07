6cb5218dc20e1b94aa415e494c47dbbd14720562
Sahil Hindwani <sahil.hindwani@harness.io>
2022-03-07 22:09:49 +0530

    [PIE-0]: Fix test

--- 870-orchestration/src/test/java/io/harness/engine/expressions/NodeExecutionsCacheTest.java
+++ 870-orchestration/src/test/java/io/harness/engine/expressions/NodeExecutionsCacheTest.java
@@ -52,7 +52,7 @@ public class NodeExecutionsCacheTest extends CategoryTest {
   // Have fieldsIncluded to be parentId and fieldExcluded to be Id, so that it uses mongoIndex and no documents are
   // queried in Mongo.
   public void testFindAllChildrenUsingRequiredProjection() {
-    doReturn(Collections.singletonList(NodeExecution.builder().identifier("test").build()))
+    doReturn(Collections.singletonList(NodeExecution.builder().identifier("test").status(Status.ABORTED).build()))
         .when(nodeExecutionService)
         .findAllChildrenWithStatusIn("PLAN_EXECUTION_ID", "PARENT_ID", null, false, true,
             Sets.newHashSet(NodeExecutionKeys.parentId, NodeExecutionKeys.status), Collections.emptySet());
