4bbffd7154b377c1d30f1b6d8fd14e56f6efea2f
Boopesh Shanmugam <boopesh.shanmugam@harness.io>
2022-03-08 16:40:11 +0530

    fix: [PL-21765]: Added proper Error messages when Request is not containing the mandatory fields.

--- 970-ng-commons/src/test/java/io/harness/utils/FullyQualifiedIdentifierHelperTest.java
+++ 970-ng-commons/src/test/java/io/harness/utils/FullyQualifiedIdentifierHelperTest.java
@@ -7,8 +7,6 @@
 
 package io.harness.utils;
 
-import static io.harness.rule.OwnerRule.*;
-
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -17,13 +15,14 @@ import io.harness.CategoryTest;
 import io.harness.category.element.UnitTests;
 import io.harness.exception.InvalidRequestException;
 import io.harness.rule.Owner;
+import io.harness.rule.OwnerRule;
 
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 public class FullyQualifiedIdentifierHelperTest extends CategoryTest {
   @Test
-  @Owner(developers = DEEPAK)
+  @Owner(developers = OwnerRule.DEEPAK)
   @Category(UnitTests.class)
   public void getFullyQualifiedIdentifier() {
     String accountIdentifier = "accountIdentifier";
@@ -44,7 +43,7 @@ public class FullyQualifiedIdentifierHelperTest extends CategoryTest {
   }
 
   @Test
-  @Owner(developers = NAMAN)
+  @Owner(developers = OwnerRule.NAMAN)
   @Category(UnitTests.class)
   public void testNoAccountIdentifierSentThrowsException() {
     String connectorIdentifier = "connectorIdentifier";
@@ -54,7 +53,7 @@ public class FullyQualifiedIdentifierHelperTest extends CategoryTest {
   }
 
   @Test
-  @Owner(developers = BOOPESH)
+  @Owner(developers = OwnerRule.BOOPESH)
   @Category(UnitTests.class)
   public void testNoOrgIdentiferForPrjectConnectorThrowsException() {
     String connectorIdentifier = randomAlphabetic(7);
