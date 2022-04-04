50c9a308bd23aad614c2f318d7cda35d13659bf9
kanikamittal09 <kanika.mittal@harness.io>
2022-04-04 17:38:50 +0530

    input

--- scripts/jenkins/buf_package.sh
+++ scripts/jenkins/buf_package.sh
@@ -2,13 +2,13 @@
 curl -L -o buf1 https://github.com/bufbuild/buf/releases/download/v1.1.1/buf-Linux-x86_64
 chmod +x buf1 
 ./buf1 --version 
-git fetch origin packageTest2
-git checkout packageTest2
+git fetch origin $1
+git checkout $1
 git status 
 EXIT_CODE=0
 output=""
-IFS=$'\n' bufOut=($(./buf1 breaking --against '.git#branch=packageTest'))
-for m in $(git log packageTest..packageTest2 --format=format:%H )
+IFS=$'\n' bufOut=($(./buf1 breaking --against '.git#branch=packageTest' --error-format=text))
+for m in $(git log packageTest..$1 --format=format:%H )
 do
     echo $m
     for k in $(git show --pretty="" --name-only $m | grep \.proto$)
@@ -25,6 +25,7 @@ do
                 EXIT_CODE=100
             fi
         done
-    done     
+    done  
+done   
 echo $EXIT_CODE >> exit_code.txt
 echo $output >> output.txt                                                                     
