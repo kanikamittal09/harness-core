c39a9046b085ee5c0b197e757c2c467fc0bdbdd7
kanika mittal <kanika.mittal@harness.io>
2022-07-05 15:24:34 +0530

    test openapi_diff log

--- test_script.sh
+++ test_script.sh
@@ -1,1 +1,1 @@
-java -jar bazel-bin/access-control/service/module_deploy.jar generate-openapi-spec target/access_source.json || ACCESS_CONTROL_S=$?
+java -jar with_handle_schema.jar target/120_target.json target/120_source.json --fail-on-incompatible || rc=$?
