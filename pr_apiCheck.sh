7805308cf57597c30ec75b4eb5b26694e58d23b8
kanikamittal09 <kanika.mittal@harness.io>
2022-07-06 19:24:05 +0530

    script info

--- pr_apiCheck.sh
+++ pr_apiCheck.sh
@@ -1,4 +1,9 @@
-BAZEL_ARGS="--announce_rc --keep_going --cache_test_results=yes --test_output=errors --show_timestamps --verbose_failures --remote_max_connections=1000 --remote_retries=1"
+git fetch origin $1:$1
+git checkout $1
+echo $2 
+commit_id=$(git merge-base $1 $2) 
+git checkout $2
+git checkout $commit_id
 
 echo "=============TARGET BRANCH================"
 echo "BUILD NG_MANAGER"
@@ -42,52 +47,60 @@ ls
 echo "=============GENERATE TARGET BRANCH API SPEC================"
 if [ $NG_MANAGER_T -eq 0 ]                                                                                                      
 then
+    echo "====Generating NG_Manager Target-Branch Api Spec===="
     java -jar bazel-bin/120-ng-manager/module_deploy.jar generate-openapi-spec target/120_target.json || NG_MANAGER_T=$?
 fi
 
 if [ $DASHBOARD_SERVICE_T -eq 0 ]                                                                                                      
 then
+    echo "====Generating Dashboard_Service Target-Branch Api Spec===="
     java -jar bazel-bin/290-dashboard-service/module_deploy.jar generate-openapi-spec target/290_target.json || DASHBOARD_SERVICE_T=$?
 fi
 
 if [ $CI_MANAGER_T -eq 0 ]                                                                                                      
 then
+    echo "====Generating CI-Manager Target-Branch Api Spec===="
     java -jar bazel-bin/310-ci-manager/module_deploy.jar generate-openapi-spec target/310_target.json  310-ci-manager/ci-manager-config.yml || CI_MANAGER_T=$?
 fi
 
 if [ $CE_NEXTGEN_T -eq 0 ]                                                                                                      
 then
+    echo "====Generating CE-NextGen Target-Branch Api Spec===="
     java -jar bazel-bin/340-ce-nextgen/module_deploy.jar generate-openapi-spec target/340_target.json || CE_NEXTGEN_T=$?
 fi
 
 if [ $PIPELINE_SERVICE_T -eq 0 ]                                                                                                      
 then
+    echo "====Generating Pipeline-Service Target-Branch Api Spec===="
     java -jar bazel-bin/800-pipeline-service/module_deploy.jar generate-openapi-spec target/800_target.json || PIPELINE_SERVICE_T=$?
 fi
 
 if [ $TEMPLATE_SERVICE_T -eq 0 ]                                                                                                      
 then
+    echo "====Generating Template-Service Target-Branch Api Spec===="
     java -jar bazel-bin/840-template-service/module_deploy.jar generate-openapi-spec target/840_target.json || TEMPLATE_SERVICE_T=$?
 fi
 
 if [ $PLATFORM_SERVICE_T -eq 0 ]                                                                                                      
 then
+    echo "====Generating Platform-Service Target-Branch Api Spec===="
     java -jar bazel-bin/platform-service/service/module_deploy.jar generate-openapi-spec target/platform_target.json || PLATFORM_SERVICE_T=$?
 fi
 
 if [ $ACCESS_CONTROL_T -eq 0 ]                                                                                                      
 then
+    echo "====Generating Access-Control Target-Branch Api Spec===="
     java -jar bazel-bin/access-control/service/module_deploy.jar generate-openapi-spec target/access_target.json || ACCESS_CONTROL_T=$?
 fi
 
 
 
-#BAZEL_ARGS="--announce_rc --keep_going --cache_test_results=yes --test_output=errors --show_timestamps --verbose_failures --remote_max_connections=1000 --remote_retries=1"
-git checkout $1
+echo "=============SOURCE BRANCH================"
+git checkout $3
 last_commit=$(git log -1 --format="%H")
 email_id=$(git show $last_commit | grep Author | cut -d '<' -f2- | sed 's/.$//')
 export EMAIL_ID=$email_id
-echo "=============SOURCE BRANCH================"
+
 if [ $NG_MANAGER_T -eq 0 ]
 then 
     echo "BUILD NG_MANAGER"
@@ -174,41 +187,49 @@ ls
 echo "=============GENERATE SOURCE BRANCH API SPEC================"
 if [ $NG_MANAGER_S -eq 0 ]                                                                                                   
 then
+    echo "====Generating NG-Manager Source-Branch Api Spec===="
     java -jar bazel-bin/120-ng-manager/module_deploy.jar generate-openapi-spec target/120_source.json || NG_MANAGER_S=$?
 fi
 
 if [ $CI_MANAGER_S -eq 0 ]                                                                                                    
 then
+    echo "====Generating CI-Manager Source-Branch Api Spec===="
     java -jar bazel-bin/310-ci-manager/module_deploy.jar generate-openapi-spec target/310_source.json 310-ci-manager/ci-manager-config.yml || CI_MANAGER_S=$?
 fi
 
 if [ $DASHBOARD_SERVICE_S -eq 0 ]                                                                                             
 then
+    echo "====Generating Dashboard-Service Source-Branch Api Spec===="
     java -jar bazel-bin/290-dashboard-service/module_deploy.jar generate-openapi-spec target/290_source.json || DASHBOARD_SERVICE_S=$?
 fi
 
 if [ $CE_NEXTGEN_S -eq 0 ]                                                                                              
 then
+    echo "====Generating CE-NextGen Source-Branch Api Spec===="
     java -jar bazel-bin/340-ce-nextgen/module_deploy.jar generate-openapi-spec target/340_source.json || CE_NEXTGEN_S=$?
 fi
 
 if [ $PIPELINE_SERVICE_S -eq 0 ]                                                                                             
 then
+    echo "====Generating Pipeline-Service Source-Branch Api Spec===="
     java -jar bazel-bin/800-pipeline-service/module_deploy.jar generate-openapi-spec target/800_source.json || PIPELINE_SERVICE_S=$?
 fi
 
 if [ $TEMPLATE_SERVICE_S -eq 0 ]                                                                                                      
 then
+    echo "====Generating Template-Service Source-Branch Api Spec===="
     java -jar bazel-bin/840-template-service/module_deploy.jar generate-openapi-spec target/840_source.json || TEMPLATE_SERVICE_S=$?
 fi
 
 if [ $PLATFORM_SERVICE_S -eq 0 ]                                                                                                 
 then
+    echo "====Generating Platform-Service Source-Branch Api Spec===="
     java -jar bazel-bin/platform-service/service/module_deploy.jar generate-openapi-spec target/platform_source.json || PLATFORM_SERVICE_S=$?
 fi
 
 if [ $ACCESS_CONTROL_S -eq 0 ]                                                                                                   
 then
+    echo "====Generating Access-Control Source-Branch Api Spec===="
     java -jar bazel-bin/access-control/service/module_deploy.jar generate-openapi-spec target/access_source.json || ACCESS_CONTROL_S=$?
 fi
 
@@ -311,4 +332,5 @@ then
     fi
 fi
 echo "API Backward Incompatibility issues in services :"$issues
+
 export EXIT_CODE=$exit_code
